ae73bc8cbfa891bd6922ab2c8070247ba1e69775
Kartikeya <114014244+KartikeyaLab@users.noreply.github.com>
2025-03-15 20:11:27 +0530

    File Uploaded

---
+++ js/sub main js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+  "use strict";
+
+  // Top News Slider
+  $(".tn-slider").slick({
+    autoplay: true,
+    infinite: true,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+})(jQuery);
